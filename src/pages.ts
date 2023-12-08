1ba40362f5ca47ea2e19b9a50f6c104fb9bfc784
jacky <jackywu526@gmail.com>
2023-12-08 19:57:42 +0800

    內容調整
    feat(package.json): install prettier
    
    feat(pages.ts): 新增 畫面路徑
    
    feat(images): 新增按鈕需要的素材圖片
    
    feat(Button.astro, iconButton.astro, textButton.astro): 製作按鈕的共用組件
    
    feat(test.astro): 添加新畫面供切換測試使用

---
+++ src/pages.ts
@@ -0,0 +1,28 @@
+const pages = [
+  {
+    link: '/',
+    name: '首頁',
+  },
+  {
+    link: '/test',
+    name: '測試頁面',
+  },
+  {
+    link: '#',
+    name: '測試',
+  },
+  {
+    link: '#',
+    name: '測試',
+  },
+  {
+    link: '#',
+    name: '測試',
+  },
+  {
+    link: '#',
+    name: '測試',
+  },
+];
+
+export default pages;
